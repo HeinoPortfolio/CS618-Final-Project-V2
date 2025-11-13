e4a872194bde667f31b370eabf28ca59b3b03561
HeinoPorfolio <meheino@sbcglobal.net>
2025-11-13 10:48:08 -0500

    feat: implemented the ability to create a new recipe

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -1,9 +1,20 @@
-// Function to get the recipes from the database server =========================
-// Gets the recipes from the backend--the database for the recipes ==============
+// Function to get the recipes from the database server =======================
+// Gets the recipes from the backend--the database for the recipes ============
 export const getRecipes = async (queryParams) => {
   const res = await fetch(
     `${import.meta.env.VITE_BACKEND_URL}/recipes?` +
       new URLSearchParams(queryParams),
   )
+  return await res.json()
+} // end getrecipes
+
+// API function tp create recipes in the database =============================
+export const createRecipe = async (recipe) => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/recipes`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(recipe),
+  })
+
   return await res.json()
 }
