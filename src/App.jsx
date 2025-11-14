73dfceca0110e6b03cc94b0ba148caa6029d4c85
HeinoPorfolio <meheino@sbcglobal.net>
2025-11-14 09:17:40 -0500

    chore: created login and logout frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 /* 
     File Purpose:
+
     -- To import the query client and the provider from tanstack
     -- Will import the Blog application 
     -- Creates a new query client to return information in the database
     -- Application will return the application wrapping the original application
 */
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Pages for the application =================================================
 import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
+import { Login } from './pages/Login.jsx'
 
 // Import router libray functions =============================================
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 // Create a new query client to call the backend ==============================
 const queryClient = new QueryClient()
+
+// Provide the token ==========================================================
+import { AuthContextProvider } from './contexts/AuthContext.jsx'
 
 // Create a router variable ===================================================
 const router = createBrowserRouter([
@@ -24,18 +31,20 @@ const router = createBrowserRouter([
   {
     path: '/signup',
     element: <Signup />,
-  } /*
+  },
   {
     path: '/login',
     element: <Login />,
-  },*/,
+  },
 ])
 
 // The application function ===========
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
     </QueryClientProvider>
   )
 }
