e18bb54fd0207f4dba73f718fbe118ee93a928d8
HeinoPorfolio <meheino@sbcglobal.net>
2025-11-17 15:05:24 -0500

    chore: implemented chat functionality

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,8 +1,15 @@
 import { Header } from '../components/Header.jsx'
 import { Status } from '../components/Status.jsx'
 
+// Imports for the chat room ========================================
+import { ChatRoom } from '../components/ChatRoom.jsx'
+import { useSocket } from '../contexts/SocketIOContext.jsx'
+
 // import { Link } from 'react-router-dom'
 export function Chat() {
+  // Include the status of the connection =======
+  const { status } = useSocket()
+
   return (
     <div style={{ padding: 8 }}>
       <Header />
@@ -11,6 +18,10 @@ export function Chat() {
       <hr />
       <br />
       <Status />
+      <br />
+      <hr />
+      <br />
+      {status === 'Connected' && <ChatRoom />}
     </div>
   )
 }
